0184b471be9076aac227071a3059f6c75e1c1b0e
Seif El-Din Sweilam <saifsweelam@gmail.com>
2020-06-24 20:17:36 +0200

    Get ready for heroku

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var scrapper = require('./scrapper')
 
 var indexRouter = require('./routes/index');
 
